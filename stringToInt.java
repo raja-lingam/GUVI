fab02eb061668b6f799db22ad5fe82232941a2fa
Kanitha Sivalingam <kani.sivalingam@gmail.com>
2016-06-03 23:10:08 +0530

    Create stringToInt.java

---
+++ stringToInt.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+      Scanner input=new Scanner(System.in);
+      String string=input.nextLine();
+      int x =Integer.parseInt(string);
+      System.out.println(x);
+    }
+}
