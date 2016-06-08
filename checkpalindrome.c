e5cc8cfa7f34e2fbceba8d8ec3f3870fbb1ecc1b
Kanitha Sivalingam <kani.sivalingam@gmail.com>
2016-06-09 01:17:04 +0530

    Create checkpalindrome.c

---
+++ checkpalindrome.c
@@ -0,0 +1,23 @@
+void check_palindrome(struct node *head){
+    int top=0;
+    char stack[50];
+    struct node *temp=head;
+    while(temp!=NULL){
+        stack[top++]=temp->data;
+        temp=temp->next;
+    }
+    temp=head;
+    int flag=0;
+    while(temp!=NULL){
+        if(temp->data!=stack[top--]){
+            flag=1;
+            break;
+        }
+    }
+    if(flag==1){
+        printf("Not a Palindrome");
+    }
+    else{
+        printf("Palindrome");
+    }
+}
