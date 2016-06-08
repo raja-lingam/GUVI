944da1f83a26fc73fb6b7f8b6b095898ccd0dd8d
Kanitha Sivalingam <kani.sivalingam@gmail.com>
2016-06-08 22:57:02 +0530

    Update coins.c

--- coins.c
+++ coins.c
@@ -1,24 +1,41 @@
 #include <stdio.h>
-
+int cmp(const void *a,const void *b){
+    return (*(int*)b)-(*(int*)a);
+}
 int main()
 {
-    int number,i,product;
+    int number,i,temp,num;
+    scanf("%d",&num);
+    temp=num;
     scanf("%d",&number);
-    int five=0,three=0,one=0;
-    if(number>=5)
-        five=number/5;
-    number=number%5;
-    if(number>=3)
-        three=number/3;
-    number=number%3;
-    if(number!=0){
-        printf("%d coin of 1\n",number);
+    int coin[number],denominations;
+    for(i=0;i<number;i++){
+        scanf("%d",&coin[i]);
     }
-    if(three!=0){
-        printf("%d coin of 3\n",three);
+    int sum=0;
+    qsort(coin,number,sizeof(int),cmp);
+    
+    for(i=0;i<number;i++){
+        int rem=0;
+        if(sum==num||num==0){
+            break;
+        }
+        if(num>=coin[i] && coin[i]!=0){
+            
+            rem=num/coin[i];
+            num=num%coin[i];
+            }
+            if(rem!=0){
+                denominations+=rem;
+            }
+        
+        sum=sum+(rem*coin[i]);
     }
-    if(five!=0){
-        printf("%d coin of 5\n",five);
+    if(sum==temp){
+        printf("%d",denominations);
+    }
+    else{
+        printf("Not possible");
     }
     return 0;
 }
