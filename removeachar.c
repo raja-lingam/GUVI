fcb86dce9aecef1714c1058399b34fbaebca58b5
Kanitha Sivalingam <kani.sivalingam@gmail.com>
2016-06-15 16:23:33 +0530

    Update removeachar.c

--- removeachar.c
+++ removeachar.c
@@ -4,28 +4,35 @@ int main()
 {
     char str[100];
     scanf("%s",str);
-    int j=strlen(str)-1,i,flag=0;
-    for(i=0;i<j;i++){
+    int j=strlen(str)-1,i,flag=0,ch;
+    for(i=0;i<=j;i++){
         if(str[i]!=str[j]){
-            if(flag!=1){
-                if(str[i]==str[j-1]){
-                    printf("%c",str[j]);
-                    flag=1;
-                }
-                else if(str[j]==str[i+1]){
-                    printf("%c",str[i]);
-                    flag=1;
-                }
-                else{
-                    printf("%c",str[j]);
-                    flag=1;
-                }
+            if(str[i]==str[j-1]){
+                ch=str[j];
+                flag++;
             }
-            j--;
+            else if(str[j]==str[i+1]){
+                ch=str[i];
+                flag++;
+            }
             else{
-                printf("cannot be converted\n");
+                if(i+1==j-1){
+                    flag++;
+                }
+                ch=str[j];
+                flag++;
             }
         }
+        j--;
+    }
+    if(flag==1){
+        printf("%c\n",ch);
+    }
+    else if(flag>1){
+        printf("cannot be converted\n");
+    }
+    else{
+        printf("Already a palindrome\n");
     }
     return 0;
 }
