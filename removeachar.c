69f4c6cee24b88a855c1e579781483171f3d33d4
Kanitha Sivalingam <kani.sivalingam@gmail.com>
2016-06-15 15:16:42 +0530

    Update removeachar.c

--- removeachar.c
+++ removeachar.c
@@ -4,20 +4,28 @@ int main()
 {
     char str[100];
     scanf("%s",str);
-    int j=strlen(str)-1,i;
+    int j=strlen(str)-1,i,flag=0;
     for(i=0;i<j;i++){
         if(str[i]!=str[j]){
-            if(str[i]==str[j-1]){
-                printf("%c",str[j]);
+            if(flag!=1){
+                if(str[i]==str[j-1]){
+                    printf("%c",str[j]);
+                    flag=1;
+                }
+                else if(str[j]==str[i+1]){
+                    printf("%c",str[i]);
+                    flag=1;
+                }
+                else{
+                    printf("%c",str[j]);
+                    flag=1;
+                }
             }
-            else if(str[j]==str[i+1]){
-                printf("%c",str[i]);
-            }
+            j--;
             else{
-                printf("%c",str[j]);
+                printf("cannot be converted\n");
             }
         }
-        j--;
     }
     return 0;
 }
